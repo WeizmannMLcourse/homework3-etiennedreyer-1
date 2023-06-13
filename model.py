6081ca240cf9d07d9fdddc783324571b9fedaf11
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2023-06-13 10:10:49 +0300

    testing my solution\!

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-
+from os import get_blocking
 import torch
 import numpy as np
 import torch.nn as nn
@@ -9,45 +9,62 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
 
-        #hyperparams (optional)
-        # - number of input features
-        # - number of hidden features
-        # - number of message-passing blocks
+        #hyperparams
+        self.ninput = 2  #input features
+        self.nhid = 100  #features of node embedding
+        self.npass = 3   #message-passing steps
 
         #encoding layer
-        self.encode = ## add neural network here
+        self.encode = nn.Sequential(nn.Linear(self.ninput,self.nhid),nn.ReLU())
 
         #message-passing layers
-        self.update_blocks = nn.ModuleList() #important to use a ModuleList rather than simply a list
+        self.update_blocks = nn.ModuleList()
 
-        for i in range(# number of message-passing blocks):
-            self.update_blocks.append( ## add neural network here
+        for i in range(self.npass):
+            self.update_blocks.append(
+                nn.Sequential(
+                    nn.Linear(self.nhid*2 + self.ninput,self.nhid*3),
+                    nn.BatchNorm1d(self.nhid*3),
+                    nn.ReLU(),
+                    nn.Linear(self.nhid*3,self.nhid*2),
+                    nn.BatchNorm1d(self.nhid*2),
+                    nn.ReLU(),
+                    nn.Linear(self.nhid*2,self.nhid),
+                    nn.BatchNorm1d(self.nhid),
+                )
+            )
 
         #classification layers
-        self.classify = ## add neural network here
+        self.classify = nn.Sequential(
+            nn.Linear(self.nhid,200),
+            nn.ReLU(),
+            nn.Linear(200,50),
+            nn.ReLU(),
+            nn.Linear(50,10),
+        )
        
     def forward(self,g):
         
-        #encode 2d inputs --> hidden representation
-        g.ndata['hidden rep'] = self.encode( ## input features 
+        #embed 2d inputs --> hidden representation
+        g.ndata['hidden rep'] = self.encode(g.ndata['xy'])
 
         #message passing loops
-        for i in range(#number of message-passing blocks
+        for i in range(self.npass):
 
             #calculate global representation as mean over hidden reps of nodes
-            global_rep            = dgl.mean_nodes( ## take mean over hidden reps of nodes
-            g.ndata['global rep'] = # use dgl.broadcast_nodes to put the global rep tensor onto each node
+            global_rep            = dgl.mean_nodes(g,'hidden rep')
+            g.ndata['global rep'] = dgl.broadcast_nodes(g,global_rep)
 
-            #concatenate hidden rep, global rep, and xy node features (last one optional but it helps a lot)
-            hidden_global_xy     = # ...
+            #concatenate hidden, global, and xy
+            hidden_global_xy     = torch.cat((g.ndata['hidden rep'],g.ndata['global rep'],g.ndata['xy']),dim=1)
 
             #deep learning layers
-            g.ndata['hidden rep'] = self.update_blocks #use the ith update block to update the hidden rep
+            g.ndata['hidden rep'] = self.update_blocks[i](hidden_global_xy)
 
         #the final global representation
-        global_rep = ## copy what we did above
+        global_rep = dgl.mean_nodes(g,'hidden rep')
 
         #final classification
-        output = #...
+        output = self.classify(global_rep)
 
         return output
